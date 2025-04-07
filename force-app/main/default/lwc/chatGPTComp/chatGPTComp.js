c8c147144c3fd91da1f7009cbef35ffe01139bc0
saurabh1998-create <saurabh8005@gmail.com>
2025-04-07 09:00:49 +0530

    chatGPT

--- force-app/main/default/lwc/chatGPTComp/chatGPTComp.js
+++ force-app/main/default/lwc/chatGPTComp/chatGPTComp.js
@@ -1,6 +1,52 @@
 import { LightningElement } from 'lwc';
+import geminiApiHandler from '@salesforce/apex/callToGemini.geminiApiHandler'
+
 
 export default class ChatGPTComp extends LightningElement {
 
-    
+    isLoading=false
+
+    getResponsefromGPT
+
+    searchInputFromUser=''
+
+    inputChangeHandler(event){
+
+        this.searchInputFromUser = event.target.value
+    }
+
+
+    onClickHandler(){
+
+        this.isLoading = true
+
+        geminiApiHandler({text : this.searchInputFromUser})
+        .then(detail =>{
+
+            // this.getResponsefromGPT = detail; 
+            // console.log('res grom gemini===>'+this.getResponsefromGPT);  
+            
+            // console.log(this.searchInputFromUser);
+
+            let parseResponse = JSON.parse(detail)
+
+
+
+            this.getResponsefromGPT = parseResponse.candidates[0].content.parts[0].text
+
+            
+        })
+        .catch(error=>{
+            console.log(error);
+            
+        })
+        .finally(()=>{
+            this.isLoading=false
+        })
+
+    }   
+
+
+
+
 }
