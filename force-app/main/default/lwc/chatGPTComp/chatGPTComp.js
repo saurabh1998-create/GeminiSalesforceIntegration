16c93e7d27e090d4fbc4342a5d4f9ad7e6f42361
saurabh1998-create <saurabh8005@gmail.com>
2025-04-08 09:43:17 +0530

    marked js library

--- force-app/main/default/lwc/chatGPTComp/chatGPTComp.js
+++ force-app/main/default/lwc/chatGPTComp/chatGPTComp.js
@@ -1,5 +1,7 @@
 import { LightningElement } from 'lwc';
 import geminiApiHandler from '@salesforce/apex/callToGemini.geminiApiHandler'
+import { loadScript } from 'lightning/platformResourceLoader';
+import marked from '@salesforce/resourceUrl/marked';
 
 
 export default class ChatGPTComp extends LightningElement {
@@ -9,6 +11,22 @@ export default class ChatGPTComp extends LightningElement {
     getResponsefromGPT
 
     searchInputFromUser=''
+
+    markdownInitialized = false;
+ 
+    renderedCallback() {
+        if (this.markdownInitialized) return;
+        this.markdownInitialized = true;
+ 
+        loadScript(this, marked)
+            .then(() => {
+                console.log('Marked library loaded');
+                
+            })
+            .catch(error => {
+                console.error('Error loading marked:', error);
+            });
+    }
 
     inputChangeHandler(event){
 
@@ -27,17 +45,28 @@ export default class ChatGPTComp extends LightningElement {
             // console.log('res grom gemini===>'+this.getResponsefromGPT);  
             
             // console.log(this.searchInputFromUser);
+// ======================
+            // let parseResponse = JSON.parse(detail)
 
-            let parseResponse = JSON.parse(detail)
 
 
+            // this.getResponsefromGPT = parseResponse.candidates[0].content.parts[0].text
+// ==================================        
 
-            this.getResponsefromGPT = parseResponse.candidates[0].content.parts[0].text
-
+                let parseResponse = JSON.parse(detail);
+                const rawText = parseResponse.candidates[0].content.parts[0].text;
+ 
+                this.getResponsefromGPT = rawText;
+ 
+                const html = window.marked.parse(rawText);
+                this.template.querySelector('[data-id="responseContainer"]').innerHTML = html;
             
         })
         .catch(error=>{
             console.log(error);
+
+            this.formattedResponse = [{ text: 'An error occurred.', id: Date.now() }];
+
             
         })
         .finally(()=>{
@@ -45,8 +74,57 @@ export default class ChatGPTComp extends LightningElement {
         })
 
     }   
-
-
-
-
 }
+
+
+// import { LightningElement, track } from 'lwc';
+// import { loadScript } from 'lightning/platformResourceLoader';
+// import marked from '@salesforce/resourceUrl/marked';
+// import geminiApiHandler from '@salesforce/apex/callToGemini.geminiApiHandler';
+ 
+// export default class ChatGPTComp extends LightningElement {
+//     @track getResponsefromGPT = '';
+//     searchInputFromUser = '';
+//     isLoading = false;
+//     markdownInitialized = false;
+ 
+//     renderedCallback() {
+//         if (this.markdownInitialized) return;
+//         this.markdownInitialized = true;
+ 
+//         loadScript(this, marked)
+//             .then(() => {
+//                 console.log('Marked library loaded');
+//             })
+//             .catch(error => {
+//                 console.error('Error loading marked:', error);
+//             });
+//     }
+ 
+//     inputChangeHandler(event) {
+//         this.searchInputFromUser = event.target.value;
+//     }
+ 
+//     onClickHandler() {
+//         this.isLoading = true;
+ 
+//         geminiApiHandler({ text: this.searchInputFromUser })
+//             .then(detail => {
+//                 let parseResponse = JSON.parse(detail);
+// const rawText = parseResponse.candidates[0].content.parts[0].text;
+ 
+//                 this.getResponsefromGPT = rawText;
+ 
+//                 const html = window.marked.parse(rawText);
+//                 this.template.querySelector('[data-id="responseContainer"]').innerHTML = html;
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//                 this.getResponsefromGPT = 'An error occurred while fetching the response.';
+//                 this.template.querySelector('[data-id="responseContainer"]').innerHTML = `<p>${this.getResponsefromGPT}</p>`;
+//             })
+//             .finally(() => {
+//                 this.isLoading = false;
+//             });
+//     }
+// }
